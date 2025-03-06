3ee9a247361e6f5225ce6220ff2c0c8dcb94b506
Areeba Irfan <161799302+Areeba-irfan-Github-account@users.noreply.github.com>
2025-03-06 21:33:09 +0500

    Add files via upload

---
+++ Class_Assignment/assignment_02/Comparison_Operator.py
@@ -0,0 +1,17 @@
+#Comparion Operator
+# ==, !=, >, <, >=, <=
+
+a = 10
+b = 10
+#== checked the equality of two operands
+print("a == b is", a == b)
+#!= checked the inequality of two operands
+print("a != b is", a != b)
+#> checked the greater than of two operands
+print("a > b is", a > b)
+#< checked the less than of two operands
+print("a < b is", a < b)
+#>= checked the greater than or equal to of two operands
+print("a >= b is", a >= b)
+#<= checked the less than or equal to of two operands
+print("a <= b is", a <= b)
