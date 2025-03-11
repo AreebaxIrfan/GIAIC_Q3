522e98d8177ff9c33cf0b4eaf21ebc87bb3e330a
Areeba Irfan <161799302+Areeba-irfan-Github-account@users.noreply.github.com>
2025-03-11 13:10:01 +0500

    day-03 updated

---
+++ Ramadan_Coding_Nights/Day_03_Password_generator/password-generator/main.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import random
+import string
+
+def generate_password(length, use_digits , use_special):
+    characters = string.ascii_letters
+
+    if use_digits:
+        characters += string.digits 
+
+    if use_special:
+        characters += string.punctuation
+    
+    return ''.join(random.choice(characters) for _ in range(length))
+
+st.title("Password Generator")
+length = st.slider("length of password",  min_value=6, max_value=32 , value=12)
+
+use_digits = st.checkbox("Use digits")
+use_special = st.checkbox("Use special characters")
+
+if st.button("Generate Password"):
+    password = generate_password(length, use_digits, use_special)
+    st.write(f"Genrate Password: {password}")
+
+st.write('---------------------')
