1ed3ed5ae5b71f8c1c33ed2cd6afede033003b5d
Areeba Irfan <161799302+Areeba-irfan-Github-account@users.noreply.github.com>
2025-03-19 20:16:53 +0500

    updated

---
+++ 🏛onsite-class/class-03/main.py
@@ -0,0 +1,21 @@
+raw_string=r"Hello \n \t worl\\d"
+raw_string="Hello \n \t worl\\d"
+# print(raw_string)
+# Slicing
+# in the end - minus one 
+my_string_2:str= "Hello world"
+# print(my_string_2[6:11])
+# print(dir(str))
+# print([method for method in dir(str) if '_' not is method])
+# print([method for method in dir(str) if "_" not in method])
+
+my_string_4 = "helloworld"
+# print(my_string_4.split("l"))
+
+my_string_6 :str = ""
+print(my_string_6.join(["Hello","worlr"]))
+
+my_string_7 = my_string_6.replace("hello" , "hey")
+print(my_string_7)
+my_string = 'hello world'
+print(my_string.find('l')) 
