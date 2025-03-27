534d1722533c2cadb36998a14a06362d2981df47
Areeba Irfan <161799302+Areeba-irfan-Github-account@users.noreply.github.com>
2025-03-27 23:18:18 +0500

    updated-06

--- 💻project/04_Assignment/assignments_00_to_05/00_intro_python/06_square_number.py
+++ 💻project/04_Assignment/assignments_00_to_05/00_intro_python/06_square_number.py
@@ -5,3 +5,15 @@
 # Type a number to see its square: 4
 
 # 4.0 squared is 16.0
+
+def main():
+    # Prompt the user to enter a number
+    number :float = float(input("Type a numberto see its square: "))
+
+    # Calculate the square of the number
+    square : float = number * number
+
+    print(f"{number} squared is {square}")
+
+if __name__ == '__main__':
+    main()
