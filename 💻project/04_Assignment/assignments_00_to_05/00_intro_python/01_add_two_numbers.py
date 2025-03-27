e64b997ef7f1c932f0a028282cec5bc35cb09067
Areeba Irfan <161799302+Areeba-irfan-Github-account@users.noreply.github.com>
2025-03-27 22:58:07 +0500

    updated

---
+++ 💻project/04_Assignment/assignments_00_to_05/00_intro_python/01_add_two_numbers.py
@@ -0,0 +1,21 @@
+# #Write a Python program that takes two integer inputs from the user and calculates their sum. The program should perform the following tasks:
+
+# Prompt the user to enter the first number.
+
+# Read the input and convert it to an integer.
+
+# Prompt the user to enter the second number.
+
+# Read the input and convert it to an integer.
+
+# Calculate the sum of the two numbers.
+
+# Print the total sum with an appropriate message.
+
+print("This program enter a number")
+num1 : str= input("Enter a number: ")
+num1 = int(num1)
+num2 : str= input("Enter a number: ")
+num2 = int(num2)
+sum :int = num1 + num2
+print("The sum of the two numbers is: ", sum)
