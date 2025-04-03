04ab2e94353a98b1c0bc1d23dc6330573b507dd3
Areeba Irfan <161799302+Areeba-irfan-Github-account@users.noreply.github.com>
2025-04-03 18:51:06 +0500

    updated

---
+++ project/04_Assignment/assignments_00_to_05/04_dictionaries/00_count_nums.py
@@ -0,0 +1,9 @@
+# This program counts the number of times each number appears in a list. It uses a dictionary to keep track of the information.
+def get_user_number():
+    user_number = []
+
+def main():
+
+
+if __name__ == '__main__':
+    main()
