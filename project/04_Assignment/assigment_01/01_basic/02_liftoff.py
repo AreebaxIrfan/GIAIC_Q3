fe5059362338f842266e1ee1aab77a415e7ab5e4
Areeba Irfan <161799302+Areeba-irfan-Github-account@users.noreply.github.com>
2025-04-04 20:08:54 +0500

    update

---
+++ project/04_Assignment/assigment_01/01_basic/02_liftoff.py
@@ -0,0 +1,10 @@
+# Write a program that prints out the calls for a spaceship that is about to launch. Countdown from 10 to 1 and then output Liftoff!
+import random
+def main():
+
+    for i in range(10):
+        print(i)
+    print("Liftoff!") 
+
+if __name__ == '__main__':
+    main()
