a0ae94935693f0ad94ab7bb2881e33997250c51e
Areeba Irfan <161799302+Areeba-irfan-Github-account@users.noreply.github.com>
2025-04-21 10:36:46 +0500

    updated

---
+++ 🏛onsite-class/class-06/main.py
@@ -0,0 +1,37 @@
+# def func_name():
+#     print('hello')
+# func_name()
+
+# def num(a,b):
+#     return a+b 
+
+# result = sum(1,2)
+# #result
+
+# def new_list(a:list)->list:
+#     a.append(4)
+#     print('a', id(a))
+#     return a
+
+# nums= [1,2,3]
+# result= new_list(nums)
+# print(result)
+# print(nums)
+# print()*
+
+
+# tyui
+# def my_Sum(*num):
+    
+    # return my_Sum
+    
+def add_two (x, y):
+    return x+ y
+lambda_fun= lambda x,y: x+ y
+ref = lambda_fun(45, 78)
+print(ref)
+
+
+
+
+
