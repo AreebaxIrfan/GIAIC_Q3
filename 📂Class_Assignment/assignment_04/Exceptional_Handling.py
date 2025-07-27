5daee9d8550628aefcf58a80f7ff61857d6be0d3
Areeba Irfan <161799302+AreebaxIrfan@users.noreply.github.com>
2025-07-27 13:33:03 +0500

    Update Exceptional_Handling.py

--- 📂Class_Assignment/assignment_04/Exceptional_Handling.py
+++ 📂Class_Assignment/assignment_04/Exceptional_Handling.py
@@ -1,34 +1,31 @@
-#Exceptional Handling (with try, except, else, and finally)
-#Try Block
-
+# Exception Handling (with try, except, else, and finally)
+# Try Block
 try:
     result = 10 / 0
-except:
-    print('an error occurred')
+except ZeroDivisionError as e:
+    print('An error occurred: Cannot divide by zero')
+    # raise  # Uncomment to re-raise the exception if needed
 
-#Except Block
+# Except Block
 try:
     result = 10 / 0
 except ZeroDivisionError:
     print('Cannot divide by zero')
 except Exception as e:
-    print(f'An un expected error occured :{e}')
+    print(f'An unexpected error occurred: {e}')
 
-#Else Block
-
-
+# Else Block
 try:
     result = 10 / 5
 except ZeroDivisionError:
     print('Cannot divide by zero')
 else:
-    print(f'Division successful:{result}')
+    print(f'Division successful: {result}')
 
-#Finally Block
+# Finally Block
 try:
     result = 10 / 0
 except ZeroDivisionError:
     print('Cannot divide by zero')
 finally:
-    print('This will always excute')
-
+    print('This will always execute')
