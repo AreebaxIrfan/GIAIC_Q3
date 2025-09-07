c0a74aa3e486885ad57fc46579558cfc26b0a244
Areeba Irfan <161799302+AreebaxIrfan@users.noreply.github.com>
2025-09-07 10:58:44 +0500

    Update 05_random_number.py

--- project/04_Assignment/assignments_00_to_05/03_if_statement/05_random_number.py
+++ project/04_Assignment/assignments_00_to_05/03_if_statement/05_random_number.py
@@ -1,12 +1,12 @@
-
 import random
 
-N_NUMBERS : int = 10
-MIN_VALUE : int = 1
-MAX_VALUE : int = 100
+N_NUMBERS: int = 10
+MIN_VALUE: int = 1
+MAX_VALUE: int = 100
 
 def main():
-    for i in range(N_NUMBERS):
+    for _ in range(N_NUMBERS):
         print(random.randint(MIN_VALUE, MAX_VALUE))
+
 if __name__ == '__main__':
     main()
