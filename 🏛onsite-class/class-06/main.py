68eaab8fc6f864a4653e5659dfc3115c0a34b0ba
Areeba Irfan <161799302+AreebaxIrfan@users.noreply.github.com>
2025-09-07 11:04:04 +0500

    Update main.py

--- 🏛onsite-class/class-06/main.py
+++ 🏛onsite-class/class-06/main.py
@@ -1,35 +1,20 @@
-# def func_name():
-#     print('hello')
-# func_name()
+def func_name():
+    print('hello')
+func_name()
 
-# def num(a,b):
-#     return a+b 
-
-# result = sum(1,2)
-# #result
-
-# def new_list(a:list)->list:
-#     a.append(4)
-#     print('a', id(a))
-#     return a
-
-# nums= [1,2,3]
-# result= new_list(nums)
-# print(result)
-# print(nums)
-# print()*
+def num(a,b):
+    return a+b 
 
 
-# tyui
-# def my_Sum(*num):
-    
-    # return my_Sum
+print(nums)
+print()*
     
 def add_two (x, y):
     return x+ y
 lambda_fun= lambda x,y: x+ y
 ref = lambda_fun(45, 78)
 print(ref)
+
 
 
 
